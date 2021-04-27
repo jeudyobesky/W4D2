2a1c47d387b4151785f7a0616163acb97d678869
jeudyobesky <jeudy.obesky@gmail.com>
2021-04-27 15:26:22 +0200

    Second commit

--- app.rb
+++ app.rb
@@ -4,4 +4,43 @@ Bundler.require
 require_relative 'lib/game'
 require_relative 'lib/player'
 
-binding.pry
+player1 = Player.new("Josiane")
+player2 = Player.new("José")
+
+puts ">Bienvenue dans LAST_SURVIVE"
+puts "============================"
+puts "Tu l'auras compris le but du jeu est de ... survivre!"
+while player1.life_points >= 0 || player2.life_points >= 0
+  if player1.life_points <= 0 || player2.life_points <= 0
+    break
+  end
+puts ""
+puts "Voici l'état de chaque joueur:"
+puts "#{player1.show_state}"
+puts "#{player2.show_state}"
+puts ""
+puts "---------------------"
+puts "Passons à la phase d'attaque:"
+puts ""
+player1.attacks(player2)
+  if player2.life_points <= 0
+    puts "le joueur #{player2.name} a été tué !"
+    break
+  end
+
+  player2.attacks(player1)
+  if player1.life_points <= 0
+    puts "le joueur #{player1.name} a été tué !"
+    break
+  end
+  puts "-------------------"
+
+end
+puts "Fin du LAST_SURVIVE"
+puts "-------------------"
+
+
+
+
+
+
--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,29 @@
 class Player
 attr_accessor :name, :life_points
 
-    def initialize (name, life_points)
+    def initialize (name, life_points = 10)
         @name = name
         @life_points = life_points
-        life_points = 10
     end
+
+    def show_state
+        "#{@name} a #{@life_points} points de vie"
+    end
+
+    def gets_damage(damage)
+        player_with_damage = @life_points - damage
+        @life_points = player_with_damage
+    end
+
+    def attacks(players)
+        players.life_points > 0
+        puts "#{self.name} attaque #{players.name} "
+        damage = self.compute_damage
+        players.gets_damage(damage)
+        puts" #{self.name} inflige #{damage} points de domages"
+    end
+
+    def compute_damage
+        return rand(1..6)
+    end
+end
